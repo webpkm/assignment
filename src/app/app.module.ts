629176739b583a86fad2b967cc4383ca1971bedb
Prem Kumar <prem.kumar@ibm.com>
2021-05-31 10:34:48 +0530

    Adding the login form control

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+const PASSWORD_MIN_LENGH = 6;
+const PASSWORD_MAX_LENGH = 20;
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
 
-  constructor() { }
+export class LoginComponent implements OnInit {
+  loginFormGroup: FormGroup;
+  passwordMinLength: number = PASSWORD_MIN_LENGH;
+  passwordMaxLength: number = PASSWORD_MAX_LENGH;
+
+  constructor() {
+    this.loginFormGroup = new FormGroup({
+      username: new FormControl('', [
+        Validators.required
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(PASSWORD_MIN_LENGH),
+        Validators.minLength(PASSWORD_MAX_LENGH)
+      ]),
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  onLoginSubmit() {
+    console.log(this.loginFormGroup.value);
+
+    // Check if login form is valid then hit the login service
+    if (this.loginFormGroup.valid) {
+
+    } else {
+      alert('Please enter correct Username or Password');
+    }
   }
 
 }
